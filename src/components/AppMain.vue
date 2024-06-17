0cbe17071ada1f90b96f773eca3d454a0f4dd553
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-06-17 16:06:58 +0200

    Aggiunte immagini bandiere

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -50,4 +50,5 @@ export default {
 </template>
 
 <style scoped>
+
 </style>
--- src/components/MainFilms.vue
+++ src/components/MainFilms.vue
@@ -21,10 +21,25 @@ export default {
                 <p>{{ films.original_title }}</p>
                 <p>{{ films.vote_average }}</p>
                 <p>{{ films.original_language }}</p>
+                <span class="lang-icon lang-icon-en"></span>
+                <span class="lang-icon lang-icon-aqc"></span>
+                <span class="lang-icon lang-icon-cs"></span>
+                <span class="lang-icon lang-icon-de"></span>
             </li>
         </ul>
     </section>
 </template>
 
 <style lang="scss" scoped>
+    @use '../assets/lang-flags.scss' as*;
+    .lang-icon {
+        background-image: url(../assets/lang-flags.png);
+    }
+    .lang-icon {
+    width: 25px;
+    height: 15px;
+    display: inline-block;
+    background-image: url('../assets/lang-flags.png');
+    background-size: auto;
+}
 </style>
